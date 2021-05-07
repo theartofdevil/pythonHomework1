32d9517ac3ddf046cc0e0fbe0814da6e5b747eea
C.Miraç Ünlü <73364018+theartofdevil@users.noreply.github.com>
2021-05-07 14:14:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,25 +20,7 @@ else:
     print ("the number that you ve entered is NOT a prime number")
 """
 ### Part 3
-
 """
-def cleaned():
-        first_string = "Ah5me4t"
-        second_string = "M9eHm4eT"
-        third_string = "Ha3K5a1n"
-        combined_value = " "
-        # Remove numbers and capitalize only first letter
-        first_string = ''.join([i for i in first_string if not i.isdigit()]).capitalize()
-        second_string = ''.join([i for i in second_string if not i.isdigit()]).capitalize()
-        third_string = ''.join([i for i in third_string if not i.isdigit()]).capitalize()
-        # combine the strings
-        combined_value = first_string + "-" + second_string + "-" + third_string
-        # Return result
-        print(combined_value)
-cleaned()
-"""
-
-
 def cleaned():
     first_string = "Ah5me4t"
     second_string = "M9eHm4eT"
@@ -55,3 +37,4 @@ def cleaned():
 
 
 cleaned()
+"""
